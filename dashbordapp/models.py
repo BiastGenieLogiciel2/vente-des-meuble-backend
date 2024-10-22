5804f8d38989effdfb920a5b841f30bd5ea1f8e6
Lucasstive1 <stivelucas037@gmail.com>
2024-10-22 09:53:12 +0200

    creation du model create

--- dashbordapp/models.py
+++ dashbordapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class create(models.Model):
+    nom = models.CharField(max_length=150)
+    email = models.EmailField()
+    password = models.CharField(max_length=200)
