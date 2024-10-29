de7b2701a56e177aff37bc7dfc30247a879d4ab1
Lucasstive1 <stivelucas037@gmail.com>
2024-10-29 06:36:06 +0100

    test de rest-framework

--- AMO/urls.py
+++ AMO/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from dashbordapp.views import TestView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TestView.as_view(), name="testviews"),
 ]
+
